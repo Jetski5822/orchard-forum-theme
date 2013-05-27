5338633d9f6e6fb494241a3c5c6c9179305177aa
Nicholas Mayne <Jetski5822@hotmail.com>
2013-05-27 17:46:29 +0100

    Makrdown + tinymce editor scripts plus inline eidtor fields

---
+++ Scripts/orchard-inlineedit-markdowneditor.js
@@ -0,0 +1,60 @@
+﻿(function ($, window, inlineedit, markdown) {
+    "use strict";
+
+    var $inlineedit = $(inlineedit);
+
+    $inlineedit.bind(inlineedit.events.retrieveContent, function (event, scope, shapeEditor) {
+        console.debug('Processing Content: TinyMce Editor');
+
+        console.debug('Processed Content: TinyMce Editor');
+    });
+
+    $inlineedit.bind(inlineedit.events.editorPrepared, function (event, shape, shapeEditor) {
+        console.debug('Finalizing Editor');
+        var marker = '<!-- markdown -->';
+        var converter = markdown.getSanitizingConverter();
+
+        var editors = $(shapeEditor).closest('.shape').find('.wmd-input');
+
+        editors.each(function () {
+
+            console.debug('Foo Bar');
+
+            var idPostfix = $(this).attr('id').substr('wmd-input'.length);
+
+            var innerEditor = new markdown.Editor(converter, idPostfix, {
+                handler: function () { window.open("http://daringfireball.net/projects/markdown/syntax"); }
+            });
+            
+            innerEditor.run();
+        });
+        
+        console.debug('Finalized Editor');
+    });
+
+    $inlineedit.bind(inlineedit.events.notifyEditorChanged, function () {
+        var converter = markdown.getSanitizingConverter();
+        
+        var innerEditor = new markdown.Editor(converter);
+        console.log(innerEditor);
+        innerEditor.run();
+    });
+
+    var editor = {
+        
+    };
+
+    if (!window.orchard) {
+        window.orchard = {};
+    }
+
+    if (!window.orchard.inlineedit) {
+        window.orchard.inlineedit = {};
+    }
+
+    if (!window.orchard.inlineedit) {
+        window.orchard.inlineedit.ui = {};
+    }
+
+    window.orchard.inlineedit.defaulteditor = editor;
+})(jQuery, window, window.orchard.inlineedit.ui, window.Markdown);
---
+++ Scripts/orchard-inlineedit-tinemceeditor.js
@@ -0,0 +1,56 @@
+﻿(function ($, window, inlineedit, tinyMCE) {
+    "use strict";
+
+    var $inlineedit = $(inlineedit);
+
+    $inlineedit.bind(inlineedit.events.retrieveContent, function (event, scope, shapeEditor) {
+        console.debug('Processing Content: TinyMce Editor');
+
+        if (tinyMCE != undefined) {
+            tinyMCE.triggerSave();
+        }
+
+        console.debug('Processed Content: TinyMce Editor');
+    });
+
+    $inlineedit.bind(inlineedit.events.editorPrepared, function (event, shape, shapeEditor) {
+        console.debug('Finalizing Editor');
+        
+        // TODO: http://blog.mirthlab.com/2008/11/13/dynamically-adding-and-removing-tinymce-instances-to-a-page/
+
+        tinyMCE.init({
+            theme: "simple",
+            mode: "specific_textareas",
+            editor_selector: "tinymce",
+            plugins: "fullscreen,autoresize,searchreplace,inlinepopups,imageeditor",
+            theme_advanced_toolbar_align: "left",
+            theme_advanced_buttons1: "search,replace,|,cut,copy,paste,|,undo,redo,|,link,unlink,charmap,emoticon,codeblock,|,bold,italic,|,numlist,bullist,formatselect,|,code,fullscreen,",
+            convert_urls: false,
+            valid_elements: "*[*]",
+            // shouldn't be needed due to the valid_elements setting, but TinyMCE would strip script.src without it.
+            extended_valid_elements: "script[type|defer|src|language]",
+            theme_advanced_resizing: true,
+            theme_advanced_resizing_use_cookie: false,
+        });
+
+        console.debug('Finalized Editor');
+    });
+
+    var editor = {
+        
+    };
+
+    if (!window.orchard) {
+        window.orchard = {};
+    }
+
+    if (!window.orchard.inlineedit) {
+        window.orchard.inlineedit = {};
+    }
+
+    if (!window.orchard.inlineedit) {
+        window.orchard.inlineedit.ui = {};
+    }
+
+    window.orchard.inlineedit.defaulteditor = editor;
+})(jQuery, window, window.orchard.inlineedit.ui, window.tinyMCE);
